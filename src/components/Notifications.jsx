cf07576436115274e88c107bbd4fb2f703d48de8
khairallah17 <med.khairallah3@gmail.com>
2023-07-22 09:20:42 +0100

    add push notifications

---
+++ src/components/Notifications.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import toast from "react-hot-toast"
+
+const Notifications = ({ toastId }) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Notifications
