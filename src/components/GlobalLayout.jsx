e1aeb03c884b4454299cc7f47135cd001434decd
khairallah17 <med.khairallah3@gmail.com>
2023-07-22 09:20:15 +0100

    update global layout

--- src/components/GlobalLayout.jsx
+++ src/components/GlobalLayout.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
+import { BiLogOut } from "react-icons/bi"
 
 const GlobalLayout = ({ children }) => {
 
     const sideBar = [{name: "Employees", to: "/"},
                      {name: "Signatures", to: "/signatures/details"},
                      {name: "Campaigns", to: "/campaigns"},
-                     {name: "Company Details", to:"/company/details"},
-                     {name: "employee fields", to: "/employee/fields"},
                      {name: "Domain Settings", to:"/domain/settings"}]
 
   return (
     <div className='flex w-screen h-screen'>
 
-        <div className="sidebar bg-primary text-white">
+        <div className="sidebar bg-primary text-white flex flex-col justify-between h-full">
 
             <ul className='flex flex-col w-44 max-w-44 min-w-44'>
                 {   
@@ -24,6 +22,11 @@ const GlobalLayout = ({ children }) => {
                     ))
                 }   
             </ul>
+
+            <Link to='../' className='w-full flex items-center gap-3 p-3 border-l-8 border-transparent cursor-pointer duration-200 hover:text-yellow-500 hover:border-yellow-500 hover:bg-white hover:gap-4'>
+                <BiLogOut className='text-xl'/>
+                logout
+            </Link>
 
         </div>
 
